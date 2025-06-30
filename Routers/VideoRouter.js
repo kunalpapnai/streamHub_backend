3cb75a059173042728826f2d1bc38c24465af5f7
Kunal Papnai <kunalpapnai123@gmail.com>
2025-06-30 23:21:25 +0530

    videos and thumbnails added

--- Routers/VideoRouter.js
+++ Routers/VideoRouter.js
@@ -3,11 +3,13 @@ const VideoRouter = express.Router();
 const {
     getVideoStream,
     getAllVideos,
+    getThumbnail,
 } = require("../Controllers/VideoController");
 
 
 /***********routes**************/
 VideoRouter.get("/", getAllVideos);
 VideoRouter.get("/watch", getVideoStream);
+VideoRouter.get("/thumbnail", getThumbnail);
 
 module.exports = VideoRouter;
